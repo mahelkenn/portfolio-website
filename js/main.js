4f5b85d29276cb287fbc9825990dda6cf1cb6b50
Michele <mahelkenn@gmail.com>
2021-05-13 22:22:25 -0800

    Added resizing to portfolio image links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Create event listener to resize and change color of links when mouse is over them //
 const link = document.querySelectorAll("#link");
 
 link.forEach(function(l, index) {
@@ -9,4 +10,18 @@ link.forEach(function(l, index) {
         l.style.color = "white";
         l.style.fontSize = "1em";    
     });
-})
+});
+
+// Create event listener to resize portfolio images when mouse is over them //
+const portfolio = document.querySelectorAll("#portfolio");
+
+portfolio.forEach(function(image, index) {
+    image.addEventListener("mouseover", function() {
+        image.style.width = "500px";
+        image.style.height = "auto";
+    });
+    image.addEventListener("mouseleave", function() {
+        image.style.width = "400px";
+        image.style.height = "auto";
+    });
+});
